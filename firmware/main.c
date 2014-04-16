f6a7ce1c8885c330b2651ddb7fdd04756abff458
unknown <mbrejza@gmail.com>
2014-04-16 23:22:10 +0100

    changes sample rates

--- firmware/main.c
+++ firmware/main.c
@@ -7,21 +7,28 @@
 #include <stdio.h>
 
 void accurate_clk(void);
-void init_i2c(void);
-void send_debug(void);
+void init(void);
+void send_debug(char*);
 
 #define BITHIGH WriteSingleReg(FREQ0,    0x3E)
 #define BITLOW  WriteSingleReg(FREQ0,    0x3C)
 
+#define ODR_HIGH  cntrl_reg1_val = 0xA1; EEPROM_ByteWrite(0x26,cntrl_reg1_val)
+#define ODR_MED   cntrl_reg1_val = 0xB1; EEPROM_ByteWrite(0x26,cntrl_reg1_val)
+#define ODR_LOW   cntrl_reg1_val = 0xB9; EEPROM_ByteWrite(0x26,cntrl_reg1_val)
+
+
+
 #define LAUNCH_DET_LEN (4)
 #define AVERAGE_BUF_LEN (16)
 
-							//100 is sample rate at max speed
-#define THRES_NO_LAUNCH (5 *100)
-#define THRES_POST_LAUNCH (1 *100)
+							//16 is sample rate at max speed
+#define THRES_NO_LAUNCH (5 *16)
+#define THRES_POST_LAUNCH (20)
 
 #define THRES_MED (2 *16)
-#define THRES_LARGE (6 *16)
+//#define THRES_LARGE (6 *16)
+#define THRES_LARGE (20)
 
 volatile int prev_bit = 0;
 char sendBuff[100] = "\0";
@@ -31,6 +38,7 @@ volatile char* sendPtr = &sendBuff[0];
 char outvar;
 
 volatile uint8_t state = 0;   //0-idle, 1-inflight, 2-postflight
+uint8_t cntrl_reg1_val = 0xB8 | 1;
 
 /*
  * main.c
@@ -39,55 +47,18 @@ void main(void) {
 	
 	WDTCTL = WDTPW + WDTHOLD;
 	WDTCTL = WDTPW + (0x2 << 5) + 3;
-	radio_init();
-	accurate_clk();
 
-	//setup counting timer
-	TA1CCR0 = 636;//636; //106; //14	//temporary top
-	TA1CTL = (0x1 << 8) | (0x3 << 6); //ACLK, upto CCR0 mode, /8 prescaler,
-//	TA1CCTL0 = CCIE;
-	TA1R = 0;
-	TA1CTL |= (0x1 << 4);
-
-	init_i2c();
+	init();
 
 	__bis_SR_register(GIE);
 
 	__delay_cycles(400000);
 
-	radio_high_power();
-	radio_carrier_on();
-
-while(1)
-	{
-unsigned char read_val[150];
-	//while(P3IN & BIT4);
-		while(P3IN & BIT4);
-		//EEPROM_SequentialRead(1,&read_val[1],5);
-		//__bis_SR_register(LPM0_bits + GIE); //sleep until interrupt fires
+	//radio_high_power();
+	//radio_carrier_on();
 
 
-		EEPROM_SequentialRead(1,&read_val[1],5);
-	// read_val[0] = EEPROM_RandomRead(0x00);  // Read from address 0x0000
-
-	  read_val[10] = EEPROM_CurrentAddressRead();//EEPROM_RandomRead(0x0C);
-	  char temperature = read_val[4];
-	  int16_t altitude = ((int16_t)read_val[1])<<8 | ((int16_t)read_val[2]);
-	  read_val[8] = altitude;
-	//  printf("hello\n");
-
-	 // __delay_cycles(900000);
-
-	  while(*sendPtr);
-	  snprintf(sendBuff,100,"HELLO,%d,%d\n\n",altitude,read_val[3]>>4);
-	  _delay_cycles(200000);
-	  sendPtr = sendBuff;
-
-
-	}
-
-	snprintf(debugBuff,10,"hello1\n");
-	//send_debug();
+	send_debug("starting up\n");
 	state = 2;		//allows a base average to be got
 
 	int32_t launch_det_buff[LAUNCH_DET_LEN];
@@ -119,17 +90,38 @@ unsigned char read_val[150];
 
 	//all altitudes in meters x16
 	EEPROM_SequentialRead(1,&buff[0],3);
+
+	//set high to get initial ground average
+	sample_rate = 2;
+	ODR_HIGH;
+
 	while(1)
 	{
 
+		//slow down
+		/*
+		while(P3IN & BIT4);
+		EEPROM_SequentialRead(1,&buff[0],3);
+		EEPROM_ByteWrite(0x26,cntrl_reg1_val);
+		EEPROM_ByteWrite(0x26,cntrl_reg1_val | 2);
+		while(P3IN & BIT4);
+		EEPROM_SequentialRead(1,&buff[0],3);
+		EEPROM_ByteWrite(0x26,cntrl_reg1_val);
+		EEPROM_ByteWrite(0x26,cntrl_reg1_val | 2);
+		*/
 		//wait for new sample
 		while(P3IN & BIT4);
 
 		//get new value
 		EEPROM_SequentialRead(1,&buff[0],3);
+		if(sample_rate)
+		{
+			EEPROM_ByteWrite(0x26,cntrl_reg1_val);		//get a new value ASAP
+			EEPROM_ByteWrite(0x26,cntrl_reg1_val | 2);
+		}
 		int32_t alt = 0;
 		if (buff[0] & 0x80)
-			alt = 0xFFFFFFFF;	//sign extend
+			alt = 0xFFF00000;	//sign extend
 
 		alt |= ((int32_t)buff[0] << 12);
 		alt |= ((int32_t)buff[1] << 4);
@@ -153,40 +145,64 @@ unsigned char read_val[150];
 		diff1 = (alt-launch_det_buff[ldb_ptr]);
 		diff2 = (launch_det_buff[ldb_ptr] - launch_det_buff[ptr_last]);
 
-		if ((diff1 & 0x80000000)^(diff2 & 0x80000000))	//if different signs, no launch
-		{
-			launch = 0;
-			if (sample_rate == 1)		//go back to idle, no launch
-				sample_rate = 0;
-		}
-		else
-		{
-			diff1 = abs(diff1);
-			diff2 = abs(diff2);
+		uint8_t sign = 0;
+		if ((diff1 & 0x80000000)^(diff2 & 0x80000000))  //see if same sign on diffs
+			sign = 1;
 
-			if (diff2 > THRES_LARGE)    //launch happening
+		diff1 = abs(diff1);
+		diff2 = abs(diff2);
+
+		if (state < 2)
+		{
+			if (diff1 > THRES_LARGE)    //high rate launch happening
 			{
 				launch = 2;
 				launch_sample_count++;
 				sample_rate = 2;
+				ODR_HIGH;
+				send_debug("HR ");
 			}
-			else if (diff1 > THRES_MED)
+			else if (sign)	//otheriwse, if different signs, no launch
 			{
-				if (diff2 > THRES_MED)    //launch happening
+				launch = 0;
+				send_debug("DS ");
+				if (sample_rate == 1)		//go back to idle, no launch
 				{
-					launch = 1;
-					launch_sample_count++;
-					sample_rate = 2;
+					sample_rate = 0;
+					ODR_LOW;
 				}
-				else		//launch might be happening, but wait until next sample
+
+			}
+			else
+			{
+				if (diff1 > THRES_MED)
 				{
-					sample_rate = 1;
+					if (diff2 > THRES_MED)    //launch happening
+					{
+						launch = 1;
+						launch_sample_count++;
+						sample_rate = 2;
+						ODR_HIGH;
+						send_debug("MR ");
+					}
+					else		//launch might be happening, but wait until next sample
+					{
+						sample_rate = 1;
+						ODR_MED;
+						send_debug("QR ");
+					}
+				}
+				else
+				{
+					if (sample_rate == 1)      //go back to idle, no launch
+					{
+						sample_rate = 0;
+						ODR_LOW;
+					}
+					send_debug("   ");
 				}
 			}
-			else if (sample_rate == 1)      //go back to idle, no launch
-				sample_rate = 0;
 		}
-
 		//TODO: compare current against average baselevel
 
 
@@ -222,14 +238,18 @@ unsigned char read_val[150];
 		average_sum += removed;
 
 
-		snprintf(debugBuff,10,"hello2\n");
-		//ss	send_debug();
+		snprintf(debugBuff,100,"%d\n",alt);
+		send_debug(debugBuff);
+		WDTCTL = (WDTCTL & 0x00FF) | WDTPW | (0x1 << 3);
 
 		switch (state)
 		{
 			case 0:  		//idle state
 				if (launch > 0)
+				{
 					state = 1;
+					send_debug("launch detected\n");
+				}
 				break;
 			case 1:			//launch state
 				if (launch == 0 )
@@ -240,6 +260,7 @@ unsigned char read_val[150];
 					state = 2;
 					no_launch_sample_count = 0;
 					launch_sample_count = 0;
+					send_debug("launch ended\n");
 				}
 
 
@@ -253,7 +274,9 @@ unsigned char read_val[150];
 					set_level = 0;
 					max_point = 0x80000000;
 					min_point = 0x7FFFFFFF;
-
+					send_debug("entering idle\n");
+					sample_rate = 0;
+					ODR_LOW;
 				}
 				break;
 		}
@@ -276,9 +299,15 @@ unsigned char read_val[150];
 
 }
 
-void send_debug(void)
+void send_debug(char* string)
 {
-	printf(debugBuff);
+
+	while (*string)
+	{
+		while(!(UCA0IFG & UCTXIFG));
+		UCA0TXBUF = *string;
+		string++;
+	}
 
 }
 
@@ -349,32 +378,61 @@ __interrupt void Timer1_A0 (void)
 
 }
 
-void init_i2c(void)
+void init(void)
 {
+	radio_init();
+	accurate_clk();
+
+	//setup counting timer
+	TA1CCR0 = 636;//636; //106; //14	//temporary top
+	TA1CTL = (0x1 << 8) | (0x3 << 6); //ACLK, upto CCR0 mode, /8 prescaler,
+//	TA1CCTL0 = CCIE;
+	TA1R = 0;
+	TA1CTL |= (0x1 << 4);
+
+
 	//remap pins
 	PMAPPWD = 0x02D52;
 	P3MAP7 = PM_UCB0SDA;
 	P1MAP0 = PM_UCB0SCL;
 	P3MAP4 = PM_TA1CCR2A;  //bodge p3.4 to timer0 for interrupts
+
+	P2MAP0 = PM_UCA0TXD;
+
 	PMAPPWD = 0;
 	P3SEL |= (1<<7) | (1<<4); //i2c and pin bodge
-	P1SEL |= (1<<0) | (1<<7);
+	P1SEL |= (1<<0);// | (1<<7);
+
+	//temp pullups
+	P3REN |= BIT7;
+	P3OUT |= BIT7;
 
 	InitI2C(0x60);
 
-	EEPROM_ByteWrite(0x26,0x80);//0xB8);   //set OSR to 128
-	EEPROM_ByteWrite(0x13,0x07);   //enable data flags
+	EEPROM_ByteWrite(0x26,0xB8);   //set OSR to 128
+	EEPROM_ByteWrite(0x13,0x07);   //enable data flags (pressure only)
 	EEPROM_ByteWrite(0x28,0x11);   //set int active low open drain
 	EEPROM_ByteWrite(0x29,0x80);   //enable DRDY interrupt
 	EEPROM_ByteWrite(0x2A,0x80);   //DRDY interrupt to INT1
-	EEPROM_ByteWrite(0x26,0xB9);   //set active
+	EEPROM_ByteWrite(0x26,cntrl_reg1_val);   //set active
 
 	//setup pin to get drdy
 	P3REN |= BIT4;
 	P3OUT |= BIT4;
 
 	//bodge p3.4 to timer0 for interrupts
-//	TA1CCTL2 = CM_2 + CCIS_0 + SCS + CAP + CCIE;
+	TA1CCTL2 = CM_2 + CCIS_0 + SCS + CAP + CCIE;
+
+
+	//setup debug uart
+	P2SEL |= BIT0;
+	P2DIR |= BIT0;
+	UCA0CTL1 |= UCSSEL_1;
+	UCA0BR0 = 26;
+	UCA0BR1 = 0;
+	UCA0MCTL = UCBRS1;
+	UCA0CTL1 &= ~UCSWRST;
+
 
 
 
